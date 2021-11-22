a7db153f933ef88622a9134eb7804510c87d9cf5
Ryan-Kan <25820201+Ryan-Kan@users.noreply.github.com>
2021-11-22 19:41:00 +0800

    Fix clicking "back" on the website causing the file to be re-uploaded

--- app/static/resources/js/main_page.js
+++ app/static/resources/js/main_page.js
@@ -1,19 +1,17 @@
 // GET ELEMENTS
-let uploadFile = $("#upload-file");
+let uploadFileInput = $("#upload-file-input");
 let outcomeText = $("#outcome-text");
 
 // MAIN CODE
 // Detect when user selected a file to upload
-uploadFile.change(() => {
-    console.log("Changed file")
-
+uploadFileInput.change(() => {
     // Reset the outcome text box
     outcomeText.text("");
     outcomeText.removeClass("error-text");
     outcomeText.removeClass("success-text");
 
     // Check if a file has been selected
-    if (uploadFile.val() === "") {  // No file
+    if (uploadFileInput.val() === "") {  // No file
         // Show an error
         outcomeText.text("You must select a file.");
         outcomeText.addClass("error-text");
@@ -21,7 +19,7 @@ uploadFile.change(() => {
     }
 
     // Check if the file extension is OK
-    if (!ACCEPTED_FILE_TYPES.includes(uploadFile[0].files[0].name.split(".").pop().toUpperCase())) {
+    if (!ACCEPTED_FILE_TYPES.includes(uploadFileInput[0].files[0].name.split(".").pop().toUpperCase())) {
         // Show an error
         outcomeText.text(`File uploaded not of accepted file type. Accepted: ${ACCEPTED_FILE_TYPES}.`);
         outcomeText.addClass("error-text");
@@ -29,7 +27,7 @@ uploadFile.change(() => {
     }
 
     // Check if the file size is OK
-    if (uploadFile[0].files[0].size > MAX_AUDIO_FILE_SIZE["Value"]) {  // `uploadFile[0]` gets the element
+    if (uploadFileInput[0].files[0].size > MAX_AUDIO_FILE_SIZE["Value"]) {  // `uploadFileInput[0]` gets the element
         // Show an error
         outcomeText.text(`File is too big; please select a file that is smaller than ${MAX_AUDIO_FILE_SIZE["Name"]}.`);
         outcomeText.addClass("error-text");
@@ -41,7 +39,7 @@ uploadFile.change(() => {
 
     // Wrap the file in a FormData object
     let formData = new FormData();
-    formData.append("file", uploadFile[0].files[0]);
+    formData.append("file", uploadFileInput[0].files[0]);
 
     // Upload the file to server
     $.ajax({
