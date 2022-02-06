c4dcc6b65d20b738f2118afc88f4fc02f07dfcda
Ryan-Kan <25820201+Ryan-Kan@users.noreply.github.com>
2022-02-06 19:58:40 +0800

    Add Automatic Browser Opening

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 main.py
 
 Created on 2021-11-16
-Updated on 2021-11-16
+Updated on 2022-02-06
 
 Copyright © Ryan Kan
 
@@ -10,7 +10,13 @@ Description: Main python program.
 """
 
 # IMPORTS
+import webbrowser
+
 from app import app
 
 # MAIN CODE
+# Open a new browser window with the url
+webbrowser.open("http://127.0.0.1:5000/", new=1)  # Fixme: possible URL not found due to race condition with below code
+
+# Run the main app
 app.run(threaded=True)
